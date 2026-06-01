9743460cedb0dd9b84d5e983d266f07bae4d8837
ahmedsaadbadawy <ahmedzack168@gmail.com>
2026-06-01 23:33:56 +0300

    Code Refactor

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kayan_app/core/layout/my_appbar.dart';
 import 'package:kayan_app/core/utils/helpers/spaces.dart';
-
-import 'widgets/hero_section.dart';
+import 'widgets/home_categories_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,55 +9,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 0,
-        title: Row(
-          children: [
-            Image.network(
-              'https://kayan-eg.netlify.app/assets/logo_image-BCHM58D5.webp',
-              width: 60.w,
-            ),
-            Spacer(),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-            horizontalSpace(8.w),
-          ],
-        ),
-      ),
+      appBar: MyAppBar(),
       body: SafeArea(
-        child: Padding(
-          padding: myPadding,
-          child: SingleChildScrollView(
-            child: Column(
-              spacing: 6.h,
-              children: [
-                CategoryCard(
-                  title: 'Find Your Dream Home',
-                  subTitle:
-                      'Discover your dream home with our curated property listings.',
-                  image:
-                      'https://kayan-eg.netlify.app/assets/hero_section_image-CN7sSwXX.webp',
-                  onTap: () {},
-                ),
-                CategoryCard(
-                  title: 'Our Finishes',
-                  subTitle:
-                      'Every detail can be fully customized to match your personal taste.',
-                  image:
-                      'https://kayan-eg.netlify.app/assets/blog_image3-Boe7FkkG.webp',
-                  onTap: () {},
-                ),
-                CategoryCard(
-                  title: 'Our Experts Tips',
-                  subTitle:
-                      'Transformed luxury living that effortlessly merges comfort with style.',
-                  image:
-                      'https://kayan-eg.netlify.app/assets/blog_image2-Cl_xLFF3.webp',
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
+        child: Padding(padding: myPadding, child: HomeCategoriesSection()),
       ),
     );
   }
